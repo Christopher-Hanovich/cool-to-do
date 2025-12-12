98626befc95738c110250e2a2ed24dbd033b83eb
AxelArredondo <axel.arredondotapia@edu.sait.ca>
2025-12-12 09:57:42 -0700

    changed firebase config

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -13,12 +13,12 @@ import { getFirestore } from "firebase/firestore";
 //
 //
 const firebaseConfig = {
-  apiKey: "AIzaSyBXsLwIOgezM2_sLmkBA-t-BosTDp6ADJE",
-  authDomain: "cool-to-do-d2069.firebaseapp.com",
-  projectId: "cool-to-do-d2069",
-  storageBucket: "cool-to-do-d2069.firebasestorage.app",
-  messagingSenderId: "81453333484",
-  appId: "1:81453333484:web:261a157cafb89dcaf59ec9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 //
 //
